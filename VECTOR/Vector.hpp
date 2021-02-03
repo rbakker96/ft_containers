c2a0b1cc9a13c668444c3fd6135685363beafa48
Roy Bakker <57988535+rbakker96@users.noreply.github.com>
2021-02-03 19:35:13 +0100

    addition to vector

--- VECTOR/Vector.hpp
+++ VECTOR/Vector.hpp
@@ -48,37 +48,41 @@ namespace ft {
     public:
         // ----------------------------------------- CONSTRUCTOR / DESTRUCTOR ------------------------------------------
         //-> Constructs an empty container, with no elements.
-        explicit vector (const allocator_type& alloc = allocator_type()) : _allocator(alloc), _used(0), _capacity(1) {
-            _array = new value_type[1];
-        };
+        explicit vector (const allocator_type& alloc = allocator_type()) : _array(NULL), _allocator(alloc), _used(0), _capacity(0) {};
 
         //-> Constructs a container with n elements. Each element is a copy of val.
-//        explicit vector (size_type n, const value_type& val = value_type(),
-//                         const allocator_type& alloc = allocator_type()) : _allocator(alloc), _used(0), _capacity(n) {
-//            _array = new value_type[n];
-//            //assign(n, val)
-//        };
+        explicit vector (size_type n, const value_type& val = value_type(),
+                         const allocator_type& alloc = allocator_type()) : _allocator(alloc), _used(0), _capacity(n) {
+            _array = new value_type[n];
+            assign(n, val);
+        };
 
         //-> Constructs a container with as many elements as the range [first,last), with each element
         //   constructed from its corresponding element in that range, in the same order.
-//        template <class InputIterator>
-//        vector (InputIterator first, InputIterator last,
-//                const allocator_type& alloc = allocator_type()) : _allocator(alloc), _used(0), _capacity(1) {
-//            _array = new value_type[1];
-//            //assign(first, last);
-//        };
+        template <class InputIterator>
+        vector (InputIterator first, InputIterator last,
+                const allocator_type& alloc = allocator_type()) : _allocator(alloc), _used(0), _capacity(0) {
+            assign(first, last);
+        };
 
         //-> Constructs a container with a copy of each of the elements in x, in the same order.
-//        vector (const vector& x) : _allocator(x._allocator), _used(x._used), _capacity(x._capacity) {
-//            _array = new value_type[x._capacity];
-            //assign(x.begin(), x.end())
-//        };
+        vector (const vector& x) : _allocator(x._allocator), _used(x._used), _capacity(x._capacity) {
+            _array = new value_type[x._capacity];
+            assign(x.begin(), x.end());
+        };
 
-        //-> Destructor
-        ~vector() {};
+        //-> Destroys the container object.
+        ~vector() {clear();};
 
-        //-> Assignment operator
-//        vector& operator= (const vector& x) {};
+        //-> Assigns new contents to the container, replacing its current contents, and modifying its size accordingly.
+        vector& operator= (const vector& x) {
+            clear();
+            _array = new value_type[x._capacity];
+            _allocator= x._allocator;
+            _used = x._used;
+            _capacity = x._capacity;
+            assign(x.begin(), x.end());
+        };
 
         // ------------------------------------------------- ITERATORS -------------------------------------------------
         //-> Returns an iterator pointing to the first element in the vector.
@@ -102,22 +106,27 @@ namespace ft {
         //-> Returns the number of elements in the vector.
         size_type size() const {return (_used);};
 
-//        //-> Returns the maximum number of elements that the vector can hold.
-//        size_type max_size() const {};
-//
-//        //-> Resizes the container so that it contains n elements.
-//        void resize (size_type n, value_type val = value_type()) {};
-//
-//        //-> Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
+        //-> Returns the maximum number of elements that the vector can hold.
+        size_type max_size() const {return((std::numeric_limits<size_type>::max() / sizeof(pointer)));};
+
+        //-> Resizes the container so that it contains n elements.
+        void resize (size_type n, value_type val = value_type()) {
+            if (n < _used)
+                _used -= (_used - n);
+            else if (n > _used)
+                push_back(val);
+        };
+
+        //-> Returns the size of the storage space currently allocated for the vector, expressed in terms of elements.
         size_type capacity() const { return (_capacity);};
 
-//        //-> Returns whether the vector is empty
-//        bool empty() const {};
-//
-//        //-> Requests that the vector capacity be at least enough to contain n elements.
+        //-> Returns whether the vector is empty
+        bool empty() const {return (_used == 0);};
+
+        //-> Requests that the vector capacity be at least enough to contain n elements.
 //        void reserve (size_type n) {};
-//
-//        //-> Requests the container to reduce its capacity to fit its size.
+
+        //-> Requests the container to reduce its capacity to fit its size.
 //        void shrink_to_fit() {};
 
 
@@ -125,16 +134,16 @@ namespace ft {
         //-> Returns a reference to the element at position n in the vector container.
 //        reference operator[] (size_type n) {};
 //        const_reference operator[] (size_type n) const {};
-//
-//        //-> Returns a reference to the element at position n in the vector.
+
+        //-> Returns a reference to the element at position n in the vector.
 //        reference at (size_type n) {};
 //        const_reference at (size_type n) const {};
-//
-//        //-> Returns a reference to the first element in the vector.
+
+        //-> Returns a reference to the first element in the vector.
 //        reference front() {};
 //        const_reference front() const {};
-//
-//        //-> Returns a reference to the last element in the vector.
+
+        //-> Returns a reference to the last element in the vector.
 //        reference back() {};
 //        const_reference back() const {};
 
@@ -142,15 +151,27 @@ namespace ft {
         // ------------------------------------------------- MODIFIERS -------------------------------------------------
         //->  the new contents are elements constructed from each of the elements in the range between first and
         //    last, in the same order.
-//        template <class InputIterator>
-//        void assign (InputIterator first, InputIterator last) {};
-//
-//        //-> the new contents are n elements, each initialized to a copy of val.
-//        void assign (size_type n, const value_type& val) {};
+        template <class InputIterator>
+        void assign (InputIterator first, InputIterator last) {
+            while (*first != *last) {
+                push_back(*first);
+                first++;
+            }
+        };
+
+        //-> the new contents are n elements, each initialized to a copy of val.
+        void assign (size_type n, const value_type& val) {
+            for (size_type i = 0; i < n; i++)
+                push_back(val);
+        };
 
         //-> Adds a new element at the end of the vector, after its current last element.
         void push_back (const value_type& val) {
-            if (_capacity == _used) {
+            if (_capacity == 0){
+                _array = new value_type [1];
+                _capacity = 1;
+            }
+            else if (_capacity == _used) {
                 pointer temp = new value_type[2 * _capacity];
                 for (size_type i = 0; i < _used; i++)
                     temp[i] = _array[i];
@@ -162,50 +183,56 @@ namespace ft {
             _used += 1;
         };
 
-//        //-> Removes the last element in the vector, effectively reducing the container size by one.
-//        void pop_back() {};
-//
-//        //->The vector is extended by inserting new elements before the element at the specified position.
+        //-> Removes the last element in the vector, effectively reducing the container size by one.
+        void pop_back() {
+            _used--;
+        };
+
+        //->The vector is extended by inserting new elements before the element at the specified position.
 //        iterator insert (iterator position, const value_type& val) {};
-//
+
 //        void insert (iterator position, size_type n, const value_type& val) {};
-//
+
 //        template <class InputIterator>
 //        void insert (iterator position, InputIterator first, InputIterator last) {};
-//
-//        //-> Removes from the vector either a single element (position) or a range of elements ([first,last)).
+
+        //-> Removes from the vector either a single element (position) or a range of elements ([first,last)).
 //        iterator erase (iterator position) {};
-//
+
 //        iterator erase (iterator first, iterator last) {};
-//
-//        //-> Exchanges the content of the container by the content of x, which is another vector object of the same type.
-//        //   Sizes may differ.
+
+        //-> Exchanges the content of the container by the content of x, which is another vector object of the same type.
+        //   Sizes may differ.
 //        void swap (vector& x) {};
-//
+
 //        //-> Removes all elements from the vector (which are destroyed), leaving the container with a size of 0.
-//        void clear() {};
-//
+        void clear() {
+            if(_capacity)
+                delete[] _array;
+        };
+
     };
 
 
     // -------------------------------------------- RELATION OPERATORS  --------------------------------------------
 //    template <class T, class Alloc>
 //    bool operator== (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
-//
+
 //    template <class T, class Alloc>
 //    bool operator!= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
-//
+
 //    template <class T, class Alloc>
 //    bool operator<  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
-//
+
 //    template <class T, class Alloc>
 //    bool operator<= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
-//
+
 //    template <class T, class Alloc>
 //    bool operator>  (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
-//
+
 //    template <class T, class Alloc>
 //    bool operator>= (const vector<T,Alloc>& lhs, const vector<T,Alloc>& rhs);
 
 };
+
 #endif //FT_CONTAINERS_VECTOR_HPP
--- cmake-build-debug/CMakeFiles/unit_test.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/unit_test.dir/cmake_clean.cmake
@@ -1,9 +1,9 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/unit_test.dir/UNIT_TEST/Bidirectionaliterator_test.cpp.o"
-  "CMakeFiles/unit_test.dir/UNIT_TEST/RandomAccessIterator_test.cpp.o"
+  "CMakeFiles/unit_test.dir/UNIT_TEST/1_Bidirectionaliterator_test.cpp.o"
+  "CMakeFiles/unit_test.dir/UNIT_TEST/2_RandomAccessIterator_test.cpp.o"
+  "CMakeFiles/unit_test.dir/UNIT_TEST/3_list_test.cpp.o"
+  "CMakeFiles/unit_test.dir/UNIT_TEST/4_vector_test.cpp.o"
   "CMakeFiles/unit_test.dir/UNIT_TEST/catch.cpp.o"
-  "CMakeFiles/unit_test.dir/UNIT_TEST/list_test.cpp.o"
-  "CMakeFiles/unit_test.dir/UNIT_TEST/vector_test.cpp.o"
   "unit_test"
   "unit_test.pdb"
 )
